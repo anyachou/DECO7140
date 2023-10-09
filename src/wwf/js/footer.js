62e51bb3c035dfc09ed0ac67c47610d451463587
Chun <fyc92330.tw@gmail.com>
2023-10-10 02:47:30 +0800

    contact us

--- src/wwf/js/footer.js
+++ src/wwf/js/footer.js
@@ -1,21 +1,25 @@
 $(document).ready(() => {
-    // Init
-    $('#friend-zone li').click(function (){
-        localStorage.setItem('friend-zone', $(this).text());
-        window.parent.location.href = '../find_friends.html'
-    });
+  // Init
+  $('#friend-zone li').click(function () {
+    localStorage.setItem('friend-zone', $(this).text());
+    window.parent.location.href = '../find_friends.html';
+  });
 
-    $('#community li').click(function (){
-        localStorage.setItem('community', $(this).text());
-        window.parent.location.href = '../community.html'
-    });
+  $('#community li').click(function () {
+    localStorage.setItem('community', $(this).text());
+    window.parent.location.href = '../community.html';
+  });
 
-    $('li').hover(
-        function() {
-            $(this).css('color', 'yellow');
-        },
-        function() {
-            $(this).css('color', '#f2f2f2');
-        }
-    );
+  $('#information-contact-us').click(function () {
+      window.parent.location.href = '../contact_us.html';
+  });
+
+  $('li').hover(
+      function () {
+        $(this).css('color', 'yellow');
+      },
+      function () {
+        $(this).css('color', '#f2f2f2');
+      }
+  );
 });
